c923d4b8333596ddc1429e570439a9d535106f80
jordangrant20 <jordan.grant20@stjohns.edu>
2025-12-15 11:39:51 -0500

    Fix local server routing for both root and demo paths

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -10,18 +10,26 @@ app.use('/api', (req, res) => {
     api_handler(req, res);
 });
 
-// Serve the main app at root and /demo
+// Serve static files from current directory
 app.use(express.static('.'));
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.get('/demo', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+// Serve main app on both root and /demo routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/demo', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 // Starting the server
 
 if (require.main === module) {
     app.listen(port, function() {
         console.log(" Server is running successfully on port " + port);
+        console.log(" Access the app at: http://localhost:" + port + "/");
+        console.log(" Access the app at: http://localhost:" + port + "/demo");
         console.log(" Access the API at: http://localhost:" + port + "/api");
-        console.log(" Access the demo at: http://localhost:" + port + "/demo");
     });
 }
 
