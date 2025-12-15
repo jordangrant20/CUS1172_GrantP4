9994ddc5210bd7241ec469dac7872c35ec48bece
jordangrant20 <jordan.grant20@stjohns.edu>
2025-12-15 10:36:46 -0500

    Consolidate all files to root directory - remove folder structure to avoid pathing issues

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -1,7 +1,7 @@
 //npm install express: terminal
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 4000;
+var port = 4000;
 
 var api_routes = require('./api_routes_dev.js');
 
@@ -9,8 +9,9 @@ app.use('/api', api_routes);
 
 app.use('/demo', express.static('front_end'));
 
-// Starting the server (only for local development)
-if (process.env.NODE_ENV !== 'production') {
+// Starting the server
+
+if (require.main === module) {
     app.listen(port, function() {
         console.log(" Server is running successfully on port " + port);
         console.log(" Access the API at: http://localhost:" + port + "/api");
