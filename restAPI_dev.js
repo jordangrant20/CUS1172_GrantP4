9dfb96ad53cbf50d480bd1ba4a107bae3689b442
jordangrant20 <jordan.grant20@stjohns.edu>
2025-12-15 23:27:55 -0500

    final product and final touches

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -1,30 +1,66 @@
 //npm install express: terminal
 var express = require('express');
 var app = express();
-var port = 4000;
 
-var api_handler = require('./api.js');
+const fs = require('fs');
+let rawdata = fs.readFileSync('./db.json');
+let employee = JSON.parse(rawdata);
 
-// Simple API route using the same serverless function
-app.use('/api', (req, res) => {
-    api_handler(req, res);
+// Define routes
+
+app.get('/api', (req, res) => {
+    let outputJSON = {
+        employees : employee["data"]
+    }
+    res.json(outputJSON);
+});
+
+app.get('/api/by_name/:qname', (req,res) => {
+    let query = req.params['qname'];
+    filtered_employees = employee["data"].filter(q => q.employee_name.includes(query));
+    let outputJSON = {
+        employees : filtered_employees
+    }
+    res.json(outputJSON);
+});
+
+app.get('/api/by_age/:start_age/:end_age', (req, res) => {
+    let start_age = req.params['start_age'];
+    let end_age = req.params['end_age'];
+    filtered_employees = employee["data"].filter(
+        q => {
+            if ((q.employee_age > parseInt(start_age)) && ( q.employee_age < parseInt(end_age))) {
+                return true;
+            }
+            return false;
+        }
+    );
+    let outputJSON = {
+        employees : filtered_employees
+    }
+    res.json(outputJSON);
 });
 
 // Serve static files from current directory
 app.use(express.static('.'));
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.get('/demo', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-// Starting the server
+// Serve main routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index_dev.html');
+});
 
-if (require.main === module) {
-    app.listen(port, function() {
-        console.log(" Server is running successfully on port " + port);
-        console.log(" Access the app at: http://localhost:" + port + "/");
-        console.log(" Access the app at: http://localhost:" + port + "/demo");
-        console.log(" Access the API at: http://localhost:" + port + "/api");
-    });
-}
+app.get('/demo', (req, res) => {
+    res.sendFile(__dirname + '/index_dev.html');
+});
 
-// Export for Vercel
-module.exports = app;
+app.get('/demo/', (req, res) => {
+    res.sendFile(__dirname + '/index_dev.html');
+});
+
+// Start the server.
+
+app.listen(4000, function() {
+    console.log("Server is running successfully on port 4000");
+    console.log(" Access the app at: http://localhost:4000/demo/");
+    console.log(" Access the API at: http://localhost:4000/api");
+});
